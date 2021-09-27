8c148c007d0d2076e1ff47dcc4f400ec492f4a0d
KhaledAlabssi <khaled.absi5@gmail.com>
2021-09-27 06:41:35 +0200

    add edit functionality for test component

--- src/Tests.js
+++ src/Tests.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 export default function Test({tests}) {
+    const [test, setTest] = useState('')
+    async function handleEdit (e) {
+        console.log(e.target.value)
+        const v = await axios.get(`http://localhost:4000/tests/${e.target.value}`)
+        await setTest(v.data[0])
+        
+        
+
+    }
     return (
         <div className='tests'>
             <Link to='/newTest'><button>Create new Test</button></Link>
             {tests.map(i => {
-                console.log(i)
+                
                 return (
                 <>
                     <h3>{i.name}</h3>
+                    <button>Assign Test</button>
+                    <button onClick={handleEdit} value={i.name}>Edit</button>
                     <hr />
                 </>
                 )
                 
             })}
-
             
         </div>
     )
