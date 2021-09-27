27f4d2e11d315b9f3b7392a94aee1891e226ac37
KhaledAlabssi <khaled.absi5@gmail.com>
2021-09-27 21:05:21 +0200

    add create student fun/component

--- src/App.js
+++ src/App.js
@@ -10,10 +10,14 @@ import NewGroup from "./NewGroup";
 import NewTest from "./NewTest";
 import Tests from './Tests'
 import Main from "./Main";
+import NewStudents from "./NewStudents";
 
 function App() {
   const [groups, setGroups] = useState([]);
   const [tests, setTests] = useState([])
+  const [currentGroup, setCurrentGroup] = useState(null)
+  const [currentTest, setCurrentTest] = useState(null)
+  const [currentTeacher, setCurrentTeacher] = useState(1)
   
 
   useEffect(() => {
@@ -59,6 +63,9 @@ function App() {
           <Route exact path='/main'>
             <Main />
           </Route>
+          <Route exact path='/newStudent'>
+          <NewStudents /> 
+          </Route>        
         </Switch>
       </Router>
     </div>
---
+++ src/NewStudents.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import React from 'react'
+
+export default function NewStudents() {
+    function handleSubmit (e) {
+        e.preventDefault();
+        axios.post('http://localhost:4000/students/new', {
+            first_name: e.target[0].value,
+            last_name: e.target[1].value,
+            username: e.target[1].value + e.target[0].value,
+            email: e.target[2].value,
+            teacher_id: 1,
+            password: 123
+
+        })
+    }
+    return (
+        <div className='newStudent'>
+            <h3>Name of Group</h3>
+            <form onSubmit={handleSubmit}>
+                <input type='text' placeholder='First Name' />
+                <input type='text' placeholder='Last Name' />
+                <input type='email' placeholder='Email' />
+                <button type='submit'>Add</button>
+            </form>
+            
+        </div>
+    )
+}
