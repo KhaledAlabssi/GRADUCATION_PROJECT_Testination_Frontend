40457afa8ed03fe7347605e91c46c46762623bb9
KhaledAlabssi <khaled.absi5@gmail.com>
2021-09-29 11:04:24 +0200

    Adding answers to add question functionality

--- src/NewQuestion.js
+++ src/NewQuestion.js
@@ -27,16 +27,39 @@ export default function NewQuestion({tests}) {
             test_id: e.target[2].value,
             question_id: currentQuestion + 1
         })
+
+        axios.post('http://localhost:4000/questions/addAnswer', {
+            correct: e.target[3].value,
+            option1: e.target[4].value,
+            option2: e.target[5].value,
+            option3: e.target[6].value,
+            question_id: currentQuestion + 1,
+            score: e.target[1].value
+            
+
+        })
     }
     return (
         <div className='newQuestion'>
             <h3>Name of Test</h3>
             <form onSubmit={handleSubmit}>
+                <h5>Question:</h5>
                 <input type='text' placeholder='Question' />
-                <input type='number' min='1' max='100' />
+                <br />
+                <input placeholder='1' type='number' min='1' max='10' step='1' />
+                <br />
                 <select name='test' id='test'>
                     {tests.map(i => <option value={i.id}>{i.name}</option>)}
                 </select>
+                <br />
+                <h5>Answer:</h5>
+                <input type='text' placeholder='Correct Answer' />
+                <input type='text' placeholder='Option 1' />
+                <input type='text' placeholder='Option 2' />
+                <input type='text' placeholder='Option 3' /> 
+                <br />
+
+
                 <button type='submit'>Add</button>
             </form>
             
